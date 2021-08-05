96f3a500b1c3102bdde5a2b91b92a4ea095363e5
Vivek Tiwary <vivek.tiwary1992@gmail.com>
2021-08-05 17:48:43 +0530

    Packer final code

---
+++ ec2-machines.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "webapp1" {
+  ami                         = var.ami
+  instance_type               = "t2.micro"
+
+  associate_public_ip_address = "true"
+  subnet_id                   = aws_subnet.PublicAZA.id
+  vpc_security_group_ids      = [aws_security_group.WebServer.id]
+  key_name                    = "2222"
+  tags = {
+    Name = "ion-WebApp1"
+  }
+
+  depends_on = [aws_internet_gateway.gw]
+  
+}
